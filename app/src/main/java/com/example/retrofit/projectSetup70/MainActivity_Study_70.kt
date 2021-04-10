a1fdb258f03367cb75baa7cd9855f68904216df8
ParkSangSun1 <appleasz1028102@gmail.com>
2021-04-10 21:18:10 +0900

    Retrofit을 사용하기 위한 준비 및 설정

---
+++ app/src/main/java/com/example/retrofit/projectSetup70/MainActivity_Study_70.kt
@@ -0,0 +1,12 @@
+package com.example.retrofit.projectSetup70
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.retrofit.R
+
+class MainActivity_Study_70 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main__study_70)
+    }
+}
