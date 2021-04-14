b3a5961a348d0392fa13172008198b4767d95217
ParkSangSun1 <appleasz1028102@gmail.com>
2021-04-14 22:58:07 +0900

    Okhttp(Logging Interceptor)

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -14,12 +14,15 @@ import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    private lateinit var retService : AlbumService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        val retService = RetrofitInstance
+        retService = RetrofitInstance
                 .getRetrofitInstance()
                 .create(AlbumService::class.java)
+        getRequesWithQueryParameters()
+        getRequesWithPathParameters()
 
 
         //path parameter example
@@ -54,6 +57,44 @@ class MainActivity : AppCompatActivity() {
                     binding.textView.append(result)
                 }
             }
+        })
+
+    }
+
+
+    private fun getRequesWithQueryParameters(){
+        val responseLiveData : LiveData<Response<Albums>> = liveData {
+            //인터페이스 사용하여 응답 개체를 가져옴
+            val response = retService.getSortedAlbums(4)
+            //방출
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if(albumsList!= null){
+                while (albumsList.hasNext()){
+                    val albumsItem = albumsList.next()
+                    Log.i("MYTAG",albumsItem.title)
+                    val result = " "+"Album Title : ${albumsItem.title}"+"\n"+
+                            " "+"Album id : ${albumsItem.id}"+"\n"+
+                            " "+"User id : ${albumsItem.userId}"+"\n\n\n"
+                    binding.textView.append(result)
+                }
+            }
+        })
+    }
+
+    private fun getRequesWithPathParameters(){
+        //path parameter example
+        val pathResponse : LiveData<Response<AlbumsItem>> = liveData {
+            val response = retService.getAlbum(3)
+            emit(response)
+        }
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_SHORT).show()
+            Log.i("ME","pathResponse 실행 ${title}")
+
         })
 
     }
--- app/src/main/java/com/example/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/example/retrofit/RetrofitInstance.kt
@@ -1,11 +1,15 @@
 package com.example.retrofit
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 //레트로핏 인스턴스 클래스
 class RetrofitInstance {
+
+
     //companion object를 추가하는 이유는 companion object 개체는 클래스가 처음 로드 될때 초기화 됩니다
     //companion object 객체 내부에 레트로핏 인스턴스를 만들면 클래스 이름을 사용하여 쉽게 사용가능
     companion object{
@@ -13,9 +17,20 @@ class RetrofitInstance {
         //http를 사용하면 안되고 https를 사용해야함(안드로이드 9.0 파이에서는 https를 사용하도록 강제)
         val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
+
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
+
+
         fun getRetrofitInstance():Retrofit{
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                    .client(client)
                 //GSON 변환기 팩토리를 사용하여 JSON을 Kotlin으로 변환합니다
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 //마지막으로 빌드
